a52dd4272e836b8ecf6a693a4b76d0d808c6123d
Thabang Kubheka <thabangk@bbd.co.za>
2023-06-23 10:54:50 +0200

    Improved game functionality

--- web/main.js
+++ web/main.js
@@ -1,17 +1,26 @@
 const cards = document.querySelectorAll('.card');
-
 const playerScoreElement = document.getElementById('player-score');
 const computerScoreElement = document.getElementById('computer-score');
+const timerElement = document.getElementById('timer');
+const resultElement = document.getElementById('result');
 
 let playerScore = 0;
 let computerScore = 0;
+let round = 0;
+let timer;
+
+function handleClick() {
+  if (timer) {
+    clearTimeout(timer);
+    timer = null;
+  }
+}
 
 cards.forEach(card => {
   card.addEventListener('click', function () {
+
     const playerChoice = this.querySelector('img').getAttribute('alt');
-
     const computerChoice = generateComputerChoice();
-
     updateSelectedCards(playerChoice, computerChoice);
 
     const winner = determineWinner(playerChoice, computerChoice);
@@ -22,10 +31,15 @@ cards.forEach(card => {
       computerScore++;
       computerScoreElement.textContent = computerScore;
     }
+    round++;
+
+    if (playerScore === 3 || computerScore === 3) {
+      endGame();
+    } else {
+      timer = startTimer();
+    }
   });
 });
-
-
 
 function generateComputerChoice() {
   const choices = ['Rock', 'Paper', 'Scissor'];
@@ -47,11 +61,6 @@ function updateSelectedCards(playerChoice, computerChoice) {
   }
 }
 
-
-
-
-
-
 function determineWinner(playerChoice, computerChoice) {
   if (playerChoice === computerChoice) {
     return 'tie';
@@ -65,3 +74,58 @@ function determineWinner(playerChoice, computerChoice) {
     return 'computer';
   }
 }
+
+function startTimer() {
+  resetTimer();
+  let time = 15;
+  timerElement.textContent = time;
+
+  return setInterval(() => {
+    time--;
+    timerElement.textContent = time;
+
+    if (time === 0) {
+      endGame();
+    }
+  }, 1000);
+}
+
+function resetTimer() {
+  clearInterval(timer);
+  timerElement.textContent = '';
+}
+
+function endGame() {
+  resetTimer();
+  cards.forEach(card => card.removeEventListener('click',handleClick));
+
+  if (playerScore > computerScore) {
+    resultElement.textContent = "Congratulations! You won the game.";
+    
+  } else if (playerScore < computerScore) {
+    resultElement.textContent = "Oops! You lost the game.";
+   
+  } else {
+    resultElement.textContent = "It's a tie!"
+  }
+  timer = startTimer();
+}
+
+function resetGame() {
+  playerScore = 0;
+  computerScore = 0;
+  round = 0;
+  resetTimer();
+  cards.forEach(card => {
+    card.removeEventListener('click', handleClick);
+    card.addEventListener('click', handleClick);
+  });
+  playerScoreElement.textContent = playerScore;
+  computerScoreElement.textContent = computerScore;
+  resultElement.textContent = '';
+  updateSelectedCards('', '');
+  timer = startTimer();
+
+}
+
+resetGame();
