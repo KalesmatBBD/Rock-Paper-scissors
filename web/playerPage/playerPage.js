03997f37ccbe56013dc59b9836fd20919af006da
Lesedi <kamoheloma@bbd.co.za>
2023-06-24 11:54:14 +0200

    playerPage

---
+++ web/playerPage/playerPage.js
@@ -0,0 +1,9 @@
+const sample = {
+    name: "Sputnik1289",
+    win: 54,
+    loss: 127,
+  };
+  
+  document.getElementById("name").innerHTML = sample.name;
+  document.getElementById("wins").innerHTML = sample.win;
+  document.getElementById("loss").innerHTML = sample.loss;
