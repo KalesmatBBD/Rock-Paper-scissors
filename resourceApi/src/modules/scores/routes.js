3e4512595b536077659433c306a63fa14a6d22dc
Thabang Kubheka <thabangk@bbd.co.za>
2023-06-24 21:38:18 +0200

    Added an end-point

--- resourceApi/src/modules/scores/routes.js
+++ resourceApi/src/modules/scores/routes.js
@@ -22,8 +22,29 @@ scoresRouter.get("/getScores", async (req, res) => {
     throw err;
   }
 });
-scoresRouter.post("", (req, res) => {
-  res.status(200).json({});
+
+
+scoresRouter.post("/postScore", async (req, res) => {
+  const { userName, score } = req.body; 
+
+  const query = `UPDATE score SET Score = ${score} WHERE UserName = '${userName}'`;
+
+  try {
+    let pool = await sql.connect(resourceApiConfig.dbConfig);
+    const request = pool.request();
+    request.query(query, (err, result) => {
+      if (err) {
+        console.error("Error updating data:", err);
+        res.status(500).json({ error: "An error occurred while updating data in the database." });
+      } else {
+        console.dir(result);
+        res.status(200).json({ message: "Score updated successfully." });
+      }
+    });
+  } catch (err) {
+    console.error("Error connecting to the database:", err);
+    throw err;
+  }
 });
 
 module.exports = {
