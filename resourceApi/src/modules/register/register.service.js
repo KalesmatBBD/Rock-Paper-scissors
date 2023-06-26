28a5d3d41368184c0fe5cab36bace1ad6721cd4d
Marius de Jager <mariusdj@bbd.co.za>
2023-06-26 21:41:03 +0200

    Added player on register

--- resourceApi/src/modules/register/register.service.js
+++ resourceApi/src/modules/register/register.service.js
@@ -1,12 +1,2 @@
-const {
-    createPlayer
-} = require('../../database/resourceApiDAL');
-
 module.exports.registerService = {
-    createPlayer:(user) => {
-        if (user && user.username) {
-            return createPlayer(user.username)
-        }
-        return Promise.reject({code: 400})
-    }
 }
